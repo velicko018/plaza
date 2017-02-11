5dc385ed4368031574d1d5a0846ee4b75e180e02
velicko018 <velicko@elfak.rs>
2017-02-11 22:06:32 +0100

    Modified RoomType model

--- src/Plaza/Models/RoomType.cs
+++ src/Plaza/Models/RoomType.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Plaza.Models
 {
     public class RoomType
     {
         public ObjectId Id { get; set; }
+
+        [BsonElement("maxGuests")]
         public int MaxGuests { get; set; }
+
+        [BsonElement("name")]
         public string Name { get; set; }
+
+        [BsonElement("totalRooms")]
         public int TotalRooms { get; set; }
+
+        [BsonElement("roomPrice")]
         public int RoomPrice { get; set; }
+
+        [BsonElement("description")]
         public string Description { get; set; }
 
     }
