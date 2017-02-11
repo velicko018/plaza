de3a6b1ca761e729a2376408f09ed2b63d80b6f3
velicko018 <velicko@elfak.rs>
2017-02-11 22:28:02 +0100

    Modified Reservation model

--- src/Plaza/Models/Reservation.cs
+++ src/Plaza/Models/Reservation.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Plaza.Models
 {
     public class Reservation
     {
-        public int NumberOfGuests { get; set; }
-        public int RoomNumber { get; set; }
-        public string UserId { get; set; }
-        public DateTime ArrivalDate { get; set; }
-        public DateTime DepartureDate { get; set; }
-        public bool Status { get; set; }
         public ObjectId Id { get; set; }
+        public string UserId { get; set; }
+        public int RoomId { get; set; }
+
+        [BsonElement("numberOfGuests")]
+        public int NumberOfGuests { get; set; }
+
+        [BsonElement("arrivalDate")]
+        public DateTime ArrivalDate { get; set; }
+
+        [BsonElement("departureDate")]
+        public DateTime DepartureDate { get; set; }
+
+        [BsonElement("status")]
+        public bool Status { get; set; }
     }
 }
