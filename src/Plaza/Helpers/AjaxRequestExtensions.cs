38cd9e3c1d2a5fec8d2f7961b86680c4fc5aed51
velicko018 <velicko@elfak.rs>
2017-02-13 19:16:08 +0100

    Add Ajax Extension

---
+++ src/Plaza/Helpers/AjaxRequestExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Plaza.Helpers
+{
+    public class AjaxRequestExtensions
+    {
+        public static bool IsAjaxRequest(HttpRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            return (request.Headers["X-Requested-With"] == "XMLHttpRequest") || ((request.Headers != null) && (request.Headers["X-Requested-With"] == "XMLHttpRequest"));
+        }
+  
+    }
+}
